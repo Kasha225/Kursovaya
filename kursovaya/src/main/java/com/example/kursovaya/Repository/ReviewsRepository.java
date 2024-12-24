1637ae047e84be106c3d6513ac5c77b1d337cf90
Kasha225 <86856944+Kasha225@users.noreply.github.com>
2024-12-24 09:06:19 +0500

    Add files via upload

---
+++ kursovaya/src/main/java/com/example/kursovaya/Repository/ReviewsRepository.java
@@ -0,0 +1,19 @@
+package com.example.kursovaya.Repository;
+
+import com.example.kursovaya.Entity.Reviews;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReviewsRepository extends JpaRepository<Reviews, Long> {
+    @Query("SELECT r FROM Reviews r WHERE r.user.id = :userId ORDER BY r.createdAt DESC")
+    List<Reviews> findByUserId(@Param("userId") Long userId);
+    @Query("SELECT r FROM Reviews r ORDER BY r.createdAt DESC")
+    Page<Reviews> findTop3Reviews(Pageable pageable);
+    @Query("SELECT r FROM Reviews r ORDER BY r.createdAt DESC")
+    Page<Reviews> findAllWithPagination(Pageable pageable);
+}
